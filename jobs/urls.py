9cb7e473576f1d826d60e2896024c3efbec96aaa
Pravallika <pravallikakudirilla77@gmail.com>
2026-07-05 20:01:16 +0530

    temporary admin creation route

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('home', views.home, name='home'),  
-    path('jobs/<int:job_id>',views.detail,name='detail'), 
+    path('jobs/<int:job_id>',views.detail,name='detail'),
+    path('create-admin/', views.create_admin, name='create_admin'), 
 ]
--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from .models import Job 
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 def home(request):
     jobs=Job.objects.all()
@@ -12,3 +14,17 @@ def home(request):
 def detail(request,job_id):
     job=get_object_or_404(Job,pk=job_id)
     return render(request,'jobs/detail.html',{'job':job})
+
+def create_admin(request):
+    username = "admin"
+
+    if User.objects.filter(username=username).exists():
+        return HttpResponse("Admin already exists!")
+
+    User.objects.create_superuser(
+        username="admin",
+        email="pravallikakudirilla77@gmail.com",
+        password="Admin@123"
+    )
+
+    return HttpResponse("Admin created successfully!")
