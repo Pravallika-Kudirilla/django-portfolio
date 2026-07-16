f9c7127b5d58ad5637820d887d4d5b168beefdd0
Pravallika <pravallikakudirilla77@gmail.com>
2026-07-16 10:21:00 +0530

    Changing Database from PostegreSQL to SQLite3

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
+#import dj_database_url
 from dotenv import load_dotenv
 import os
 
@@ -82,11 +82,19 @@ WSGI_APPLICATION = 'Portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": dj_database_url.config(
+#         default=os.getenv("DATABASE_URL")
+#     )
+# }
+
 DATABASES = {
-    "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL")
-    )
+    "default": {    
+        "ENGINE":"django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",    
+    }
 }
+
 
 
 # Password validation
